a2f6a97113232da8d116f9e64cec3357d804175f
Ray Chen <ray@raychen.io>
2026-04-22 00:55:02 +0800

    Fix CI

--- packages/inkwell/tsup.config.ts
+++ packages/inkwell/tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/index.ts"],
-	format: ["esm", "cjs"],
-	dts: true,
-	splitting: true,
-	treeshake: true,
-	clean: true,
-	external: ["react", "react-dom"],
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
+  dts: true,
+  splitting: true,
+  treeshake: true,
+  clean: true,
+  external: ["react", "react-dom"],
 });
